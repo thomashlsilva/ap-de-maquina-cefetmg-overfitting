af8dad58debbeeeedeb81e94ed1072d946314009
Thomás Silva <thomashlsilva@gmail.com>
2025-04-24 11:09:42 -0300

    Resolução das atividades

--- arvore_decisao.py
+++ arvore_decisao.py
@@ -3,6 +3,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 from typing import Tuple, List
+
+
 def cria_modelo(X:pd.DataFrame ,y:pd.Series ,min_samples_split:float):
     """
         Retorna o modelo a ser usado.
@@ -16,10 +18,10 @@ def cria_modelo(X:pd.DataFrame ,y:pd.Series ,min_samples_split:float):
     #instancia a arvore de decisão (use  a classe DecisionTreeClassifier do Scikitlearn, defina o
     #..parametro min_samples_split e random_state=1 - é muito importante manter a seed fixa para
     #..que os resultados se mantenham sempre os mesmos - reprodutibilidade)
-    decision_tree = None
+    decision_tree = DecisionTreeClassifier(min_samples_split=min_samples_split, random_state=1)
 
     #retone o modelo por meio do método fit
-    return None
+    return decision_tree.fit(X, y)
 
 def divide_treino_teste(df:pd.DataFrame, val_proporcao_treino:float) -> Tuple[pd.DataFrame,pd.DataFrame]:
     """
@@ -29,12 +31,12 @@ def divide_treino_teste(df:pd.DataFrame, val_proporcao_treino:float) -> Tuple[pd
         Retorna uma tupla com o treino e teste separados
     """
     #1. obtenha o treino usando o método sample do DataFrame
-    df_treino = None
+    df_treino = df.sample(frac=val_proporcao_treino, random_state=1)
 
     #2. Para obter o teste, selecione as instancias que estão em df e não estão em df_treino (use o método drop)
-    df_teste = None
+    df_teste = df.drop(df_treino.index)
 
-    return df_treino,df_teste
+    return df_treino, df_teste
 
 
 
@@ -52,16 +54,17 @@ def faz_classificacao(x_treino:pd.DataFrame, y_treino:pd.Series, x_teste:pd.Data
                 * np.sum soma os valores de um vetor (considerando True=1 e False=0)
     """
     #cria o modelo (use a função previamente criada)
-    model_dtree = None
+    model_dtree = cria_modelo(x_treino, y_treino, min_samples_split)
 
     #realiza a predição (use o método predict do modelo)
-    y_predicted = None
+    y_predicted = model_dtree.predict(x_teste)
 
     #calcule a acurácia
-    acuracia = None
+    acuracia = np.sum(y_predicted == y_teste) / len(y_teste)
 
 
-    return y_predicted,acuracia
+    return y_predicted.tolist(), acuracia
+
 
 def plot_performance_min_samples(X_treino,y_treino,X_teste,y_teste):
     """
@@ -84,9 +87,9 @@ def plot_performance_min_samples(X_treino,y_treino,X_teste,y_teste):
 
     for min_samples in np.arange(0.001,0.7,0.01):
         #complete a linha abaixo com a função e parametros corretos para calcular a acurácia no teste
-        y_predicted, ac_teste = None
+        y_predicted, ac_teste = faz_classificacao(X_treino, y_treino, X_teste, y_teste, min_samples)
         #complete a linha abaixo com a função e parametros corretos para calcular a acurácia no treino
-        y_predicted, ac_treino = None
+        y_predicted, ac_treino = faz_classificacao(X_treino, y_treino, X_treino, y_treino, min_samples)
 
         #adiciona a acuracia no treino, no teste e o parametro min_samples
         arr_ac_treino.append(ac_treino)
@@ -94,5 +97,12 @@ def plot_performance_min_samples(X_treino,y_treino,X_teste,y_teste):
         arr_min_samples.append(min_samples)
 
     #plota o resultado
-    plt.plot(arr_min_samples,arr_ac_treino,"b--")
-    plt.plot(arr_min_samples,arr_ac_teste,"r-")
+    plt.figure(figsize=(10, 6))
+    plt.plot(arr_min_samples, arr_ac_treino, "b--", label="Acurácia Treino")
+    plt.plot(arr_min_samples, arr_ac_teste, "r-", label="Acurácia Teste")
+    plt.xlabel("min_samples_split")
+    plt.ylabel("Acurácia")
+    plt.title("Variação da Acurácia com min_samples_split")
+    plt.legend()
+    plt.grid(True)
+    plt.show()
